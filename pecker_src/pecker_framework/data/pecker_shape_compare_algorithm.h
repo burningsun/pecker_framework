7a81b4c99bbda0da4e5ebe2909f9f42f33e4a495
cutxyz <501931049@qq.com>
2013-04-24 10:35:59 +0800

    line dot line line 2d relationship

--- pecker_src/pecker_framework/data/pecker_shape_compare_algorithm.h
+++ pecker_src/pecker_framework/data/pecker_shape_compare_algorithm.h
@@ -15,6 +15,16 @@
 
 PECKER_BEGIN
 
+enum LINE_DOT_2D_RELATIONSHIP
+{
+	DOT_IN_LINE = 0,
+	LINE_ABOVE_DOT,
+	LINE_BELOW_DOT,
+
+	LINE_DOT_2D_RELATIONSHIP_COUNT
+};
+
+
 inline Bool equal_dot2d_float(const pecker_vector2& first_shape,const pecker_vector2& second_shape)
 {
 	return (first_shape == second_shape);
@@ -109,15 +119,20 @@ inline Bool equal_ploygon2d_float(const pecker_vector2* first_shape_points,nSize
 	return true;
 }
 
+
+
+
 //////////////////////////////////////////////////////////////////////////
 //点C在线段AB上
 //1、向量AB叉乘向量AC，如果叉积等于0，则A B C三点共线             
 //2、若C在直线AB上，且C在以AB为对角线的矩形内，则C在线段AB上
-inline Bool intersect_line_dot_2d_float(const pecker_vector2& line_pointA,const pecker_vector2& line_pointB,const pecker_vector2& pointC)
+// 如何判断点C在向量AB的左侧还是右侧：AC×AB的数值大于0则在右侧，小于0则在左侧
+
+inline HEnum intersect_line_dot_2d_float(const pecker_vector2& line_pointA,const pecker_vector2& line_pointB,const pecker_vector2& pointC)
 {
 	pecker_vector2 lineAB = line_pointB - line_pointA;
 	pecker_vector2 lineAC = pointC - line_pointA;
-	Float cross_value = lineAB.cross_value(lineAC);
+	Float cross_value = lineAC.cross_value(lineAB);
 
 	if (EQFLOATZERO(cross_value))
 	{
@@ -137,14 +152,25 @@ inline Bool intersect_line_dot_2d_float(const pecker_vector2& line_pointA,const 
 		}
 		if (pointC.x >= min_x && pointC.x <= max_x && pointC.y >= min_y && pointC.y <= max_y)
 		{
-			return true;
+			return DOT_IN_LINE;
 		}	
 	}
-	return false;
+	else if (cross_value > 0)
+	{
+		return LINE_ABOVE_DOT;
+	}
+	else
+	{
+		return LINE_BELOW_DOT;
+	}
 }
-inline Bool intersect_line_dot_2d_int(SInt line_point_Ax,SInt line_point_Ay,SInt line_point_Bx,SInt line_point_By,SInt dotX,SInt dotY)
+inline HEnum intersect_line_dot_2d_int(SInt line_point_Ax,SInt line_point_Ay,SInt line_point_Bx,SInt line_point_By,SInt pointCx,SInt pointCy)
 {
-	SInt cross_value = line_point_Ax*line_point_By- line_point_Ay*line_point_Bx;
+	SInt lineABx = line_point_Bx - line_point_Ax;
+	SInt lineABy = line_point_By - line_point_Ay;
+	SInt lineACx = pointCx - line_point_Ax;
+	SInt lineACy = pointCy - line_point_Ay;
+	SInt cross_value = lineABy*lineACx - lineABx*lineACy;
 
 	if (0 == cross_value)
 	{
@@ -162,18 +188,124 @@ inline Bool intersect_line_dot_2d_int(SInt line_point_Ax,SInt line_point_Ay,SInt
 			min_y = line_point_By;
 			max_y = line_point_Ay;
 		}
-		if (pointC.x >= min_x && pointC.x <= max_x && pointC.y >= min_y && pointC.y <= max_y)
+		if (pointCx >= min_x && pointCx <= max_x && pointCy >= min_y && pointCy <= max_y)
 		{
-			return true;
+			return DOT_IN_LINE;
 		}	
 	}
-	return false;
+	else if (0 < cross_value)
+	{
+		return LINE_ABOVE_DOT;
+	}
+	else
+	{
+		return LINE_BELOW_DOT;
+	}
 }
 
-inline Bool intersect_line_line_2d_float(const pecker_vector2& line_pointA,const pecker_vector2& line_pointB,const pecker_vector2& pointC,const pecker_vector2& pointD)
+//////////////////////////////////////////////////////////////////////////
+// BitField              bitorder 31~0
+//								第13个bit标识第二直线D点在第一直线的下方
+//								第12个bit标识第二直线D点在第一直线的上方
+//								第11个bit标识第二直线C点在第一直线的下方
+//								第10个bit标识第二直线C点在第一直线的上方
+//                             第 9个bit标识两直线是否相交(不包含两直线重叠的情况)
+//								第8个bit标识两直线是否平衡
+//								第0~7个bit标识当前判断结果
+//								第14~31个bit保留
+#define  BALANCE_LINE  (1 << 8)
+#define  INTERSECT_LINE (1 << 9)
+#define  C_POINT_ABOVE (1 << 10)
+#define  C_POINT_BELOW (1 << 11)
+#define  D_POINT_ABOVE (1 << 12)
+#define  D_POINT_BELOW (1 << 13)
+#define  LINE_INTERSECT_TRUE (1)
+#define  LINE_INTERSECT_FALSE (0)
+inline BitField intersect_line_line_2d_float(const pecker_vector2& pointA,const pecker_vector2& pointB,const pecker_vector2& pointC,const pecker_vector2& pointD)
 {
+	BitField return_result = 0;
+	pecker_vector2 lineAB = pointB - pointA;
+	pecker_vector2 lineCD = pointD - pointC;
+	// 点A，C 在直线AB两侧，并且 点C，D在直线CD两侧，则线段AC与线段CD相交 (充分必要条件，两侧包含点在直线上的情况)
+	Float cross_AB_CD_value = lineAB.cross_value(lineCD);
 
-	return false;
+	if (0 == cross_AB_CD_value) //平衡，重合
+	{
+		return_result |= BALANCE_LINE;
+		Float min_x = pointA.x;
+		Float min_y = pointA.y;
+		Float max_x = pointB.x;
+		Float max_y = pointB.y;
+
+		if (min_x > max_x)
+		{
+			min_x = pointB.x;
+			max_x = pointA.x;
+		}
+		if (min_y > max_y)
+		{
+			min_y = pointB.y;
+			max_y = pointA.y;
+		}
+
+		if ((pointC.x >= min_x && pointC.x <= max_x && pointC.y >= min_y && pointC.y <= max_y) ||
+			(pointD.x >= min_x && pointD.x <= max_x && pointD.y >= min_y && pointD.y <= max_y))
+		{
+			return_result |= LINE_INTERSECT_TRUE;
+		}
+	}
+	else // 不平衡，交叉
+	{
+		return_result |= INTERSECT_LINE;
+		pecker_vector2 lineAC = pointC - pointA;
+		pecker_vector2 lineAD = pointD - pointA;
+		Float cross_AC_AB_value = lineAC.cross_value(lineAB);
+		Float cross_AD_AB_value = lineAD.cross_value(lineAB);
+		Float cross = cross_AC_AB_value * cross_AD_AB_value;
+
+		if (cross_AC_AB_value > 0)
+		{
+			return_result |= C_POINT_ABOVE;
+		}
+		else if (cross_AC_AB_value < 0)
+		{
+			return_result |= C_POINT_BELOW;
+		}
+		if (cross_AD_AB_value > 0)
+		{
+			return_result |= D_POINT_ABOVE;
+		}
+		else if (cross_AD_AB_value < 0)
+		{
+			return_result |= D_POINT_BELOW;
+		}
+		if ( cross <= 0)
+		{
+			Float min_x = pointA.x;
+			Float min_y = pointA.y;
+			Float max_x = pointB.x;
+			Float max_y = pointB.y;
+
+			if (min_x > max_x)
+			{
+				min_x = pointB.x;
+				max_x = pointA.x;
+			}
+			if (min_y > max_y)
+			{
+				min_y = pointB.y;
+				max_y = pointA.y;
+			}
+
+			if ((pointC.x >= min_x && pointC.x <= max_x && pointC.y >= min_y && pointC.y <= max_y) ||
+				(pointD.x >= min_x && pointD.x <= max_x && pointD.y >= min_y && pointD.y <= max_y))
+			{
+				return_result |= LINE_INTERSECT_TRUE;
+			}
+		}
+	}
+
+	return return_result;
 }
 
 PECKER_END
