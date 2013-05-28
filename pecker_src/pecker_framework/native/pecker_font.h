19340745d98e8afb42640a09e6c6659276711c3e
cutxyz <501931049@qq.com>
2013-05-28 21:03:40 +0800

    add pecker_font.h header file add a doc

---
+++ pecker_src/pecker_framework/native/pecker_font.h
@@ -0,0 +1,29 @@
+﻿/*
+ * pecker_font.h
+ *
+ *  Created on: 2013-5-28
+ *      Author: 李镇城  （ cut / cutxyz）
+ *		e-mail: cut-12345@hotmail.com
+ *              501931049@qq.com
+ */
+
+#ifndef PECKER_FONT_H_
+#define PECKER_FONT_H_
+
+#include "../CPeckerObject.h"
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+PECKER_BEGIN
+	
+class pecker_font
+{
+
+public:
+	
+};
+
+PECKER_END
+
+
+#endif /* PECKER_FONT_H_ */
